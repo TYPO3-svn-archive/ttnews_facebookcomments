c701d5524737157ed799fdd84aff4f96bade8fd5
checkerap <checkerap@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-25 14:30:40 +0000

    initial release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ttnews_facebookcomments/trunk@28020 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "ttnews_facebookcomments"
+#
+# Auto generated 24-12-2009 19:04
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Facebook comments for news',
+	'description' => 'Implements Facebook Comments Box for tt_news',
+	'category' => 'plugin',
+	'author' => 'Cosmin Stefaniga',
+	'author_email' => 'cosmin.stefaniga@gmail.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"8e27";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"237f";s:20:"doc/wizard_form.html";s:4:"5e22";}',
+);
+
+?>
